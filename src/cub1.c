bba326978e6bf83257b2f958383b99065eb8d67f
taegor <egorproskurin.95@gmail.com>
2021-01-30 20:24:55 +0300

    how to draw smth except pixel??

--- src/cub1.c
+++ src/cub1.c
@@ -1,63 +1,26 @@
 #include "cub3d.h"
 
-static int scale = 50;
-
-int		scale_map(t_vars *vars, t_point *point, int pass, int col)
+int		my_mlx_pp(t_data *data, int x, int y, int color)
 {
-	int	scale_y;
-	int scale_x;
-//	int scale;
+	char *dst;
 
-//	scale = 10
-	scale_y = scale + point->y;
-	scale_x = scale + point->x;
-	if (pass == 0)
-	{
-		while (point->y < scale_y)
-		{
-			point->x = scale_x - scale;
-			while(point->x < scale_x) 
-			{
-				mlx_pixel_put(vars->mlx, vars->win,
-						point->x++, point->y, col);
-			}
-			point->y++;
-		}
-		point->y = scale_y - scale;
-	}
-	else
-		point->x += scale;
-	return (1);
+	dst = data->addr + (y * data->llen + x * (data->bpp / 8));
+	*(unsigned int*)dst = color;
 }
 
-int		show_map(char **map)
+int		show_map(t_all *all)
 {
-	int		i;
-	int		j;
-	t_point	point;
-	t_vars	vars;
-	
-	i = 0;
-	j = 0;
-	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello");
+	t_data		img;
+	t_point		point;
+
+	point.x = 100;
 	point.y = 100;
-	while (map[i])
-	{
-		j = 0;
-		point.x = 100;
-		while (map[i][j])
-		{
-			if (map[i][j] == '1')
-				scale_map(&vars, &point, 0, 0xFFFFFF);
-			else if (map[i][j] == 'N')
-				scale_map(&vars, &point, 0, 100);
-			else
-				scale_map(&vars, &point, 1, 0);
-			j++;
-		}
-		point.y += scale;
-		i++;
-	}
-	mlx_loop(vars.mlx);
+	all->vars.mlx = mlx_init();
+	all->vars.win = mlx_new_window(all->vars.mlx, 640, 480, "test");
+	img.img = mlx_new_image(all->vars.mlx, 640, 480);
+	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.llen, &img.en);
+	my_mlx_pp(&img, point.x, point.y, 0xFFFFFF); 
+	mlx_put_image_to_window(all->vars.mlx, all->vars.win, img.img,0, 0);	
+	mlx_loop(all->vars.mlx);
+	
 }
--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include "cub3d.h"
 
+int		make_map(t_list **head, size_t size, t_all *all);
+
 void printt(char **strs)
 {
 	int i = 0;
@@ -10,37 +12,50 @@ void printt(char **strs)
 	}
 }
 
-char	**make_map(t_list **head, size_t size)
+int		read_map(int fd, t_all *all)
+{
+	char *line;
+	t_list *head = NULL;
+
+	while (get_next_line(fd, &line))
+	{
+//		printf ("here\n");
+		ft_lstadd_back(&head, ft_lstnew(line));	
+	}	
+	ft_lstadd_back(&head, ft_lstnew(line));	
+	make_map(&head, ft_lstsize(head), all);
+}
+
+int		make_map(t_list **head, size_t size, t_all *all)
 {
 	char **map;
 	int i;
 	t_list *tmp;
 
 	i = 0;
+//	printf ("here\n");
 	tmp = *head;
 	map = ft_calloc(sizeof(char*), size + 1);
-//	printf ("here\n");
 	while(tmp)
 	{
 		map[i++] = tmp->content;
 		tmp = tmp->next;
 	}
-	return (map);
+	i = 0;
+//	printf("here\n");
+	while (map[i++])
+		all->map[i] = map[i]; 
+	return (1);
 }
 
 int main(int argc, char *argv[])
 {
 	int fd;
-	char *line;
-	t_list *head;
+	t_all all;
 
 	fd = open(argv[1], O_RDONLY);
-	while (get_next_line(fd, &line))
-	{
-//		printf("line:%s\n", line);
-		ft_lstadd_back(&head, ft_lstnew(line));	
-	}
-	ft_lstadd_back(&head, ft_lstnew(line));	
-	show_map(make_map(&head, ft_lstsize(head)));
+	read_map(fd, &all); 
+//	printf ("here\n");
+	show_map(&all);
 	return (1);
 }
